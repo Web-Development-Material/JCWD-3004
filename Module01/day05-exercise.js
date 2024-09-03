e4c208c9c5852556546b63c0aa43ef86009e6d3f
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-09-03 09:35:30 +0700

    add exercise 05

--- Module01/day05-exercise.js
+++ Module01/day05-exercise.js
@@ -14,3 +14,39 @@ function insertToArray(maxSize, ...numbers) {
 
 const maxSize = 5;
 // console.log(insertToArray(maxSize, 5, 10, 24, 3, 6, 7, 8));
+
+// -- add unique element
+function addUniqueElement(arr, newElement) {
+  if (arr.indexOf(newElement) === -1) {
+    arr.push(newElement);
+  }
+  return arr;
+}
+
+const newElement = 4;
+const arr = [1, 2, 3, 4];
+// console.log(addUniqueElement(arr, newElement));
+
+// -- count duplicate
+function sumOfDuplicates(arr) {
+  let sum = 0;
+  let seen = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (seen[arr[i]]) {
+      seen[arr[i]]++;
+      console.log(seen);
+    } else {
+      seen[arr[i]] = 1;
+    }
+  }
+  for (let key in seen) {
+    // console.log(key);
+    if (seen[key] > 1) {
+      sum += Number(key) * seen[key];
+    }
+  }
+  return sum;
+}
+let arr2 = [10, 20, 40, 10, 50, 30, 10, 60, 10];
+console.log(sumOfDuplicates(arr2));
