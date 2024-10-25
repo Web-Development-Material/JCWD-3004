e1a42f77e37f5830d2342ae511f6b36b97df223b
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-10-25 15:45:24 +0700

    add cors

--- Module03/sayur-kita-v2-api/src/app.ts
+++ Module03/sayur-kita-v2-api/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import environment from "dotenv";
+import cors from "cors";
 import adminRouter from "./routers/admin.router";
 import userRouter from "./routers/user.router";
 import authRouter from "./routers/auth.router";
@@ -13,6 +14,11 @@ const errorHandler = new ErrorHandlerMiddleware();
 const PORT = process.env.SERVER_PORT_DEV;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use("/api/admin", adminRouter);
 app.use("/api/users", userRouter);
